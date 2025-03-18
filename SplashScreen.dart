a6a9870eb7e6dfd6601d693809ab9376a82db458
Alaa Essam <alaa.e.alhakimi@gmail.com>
2025-03-18 16:09:25 -0700

    Update SplashScreen.dart

--- SplashScreen.dart
+++ SplashScreen.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
+import '../screens/Login.dart';
+
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -89,47 +91,3 @@ class _SplashScreenState extends State<SplashScreen>
   }
 }
 
-// شاشة تسجيل الدخول
-class LoginScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white, // لون الخلفية أبيض
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("تسجيل الدخول",
-                style: TextStyle(
-                    fontSize: 24, color: Colors.black)), // عنوان الصفحة
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: TextField(
-                decoration: InputDecoration(
-                    labelText: "البريد الإلكتروني",
-                    labelStyle: TextStyle(
-                        color: Colors.black)), // حقل إدخال البريد الإلكتروني
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: TextField(
-                decoration: InputDecoration(labelText: "كلمة المرور"),
-                obscureText: true, // إخفاء النص عند الكتابة لكلمة المرور
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-                onPressed: () {},
-                child: Text("تسجيل الدخول")), // زر تسجيل الدخول
-            TextButton(
-                onPressed: () {},
-                child: Text(
-                    "أو سجل الدخول عبر Google")), // زر تسجيل الدخول عبر Google
-          ],
-        ),
-      ),
-    );
-  }
-}
